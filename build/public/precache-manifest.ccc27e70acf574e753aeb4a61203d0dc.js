024d51f3dbab33891047b7696d51f425cb1c027a
Hu Yuan Bao <nguyenbaont2212@gmail.com>
2020-07-22 18:06:34 +0700

    built server

---
+++ build/public/precache-manifest.ccc27e70acf574e753aeb4a61203d0dc.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([{
+  "revision": "5a5222d90bc0ae74074217c837fa3d5f",
+  "url": "/index.html"
+}, {
+  "revision": "f4567141c1f2da13fd3d",
+  "url": "/static/css/main.9d03a79b.chunk.css"
+}, {
+  "revision": "b25acd09865b96e23663",
+  "url": "/static/js/2.83be525c.chunk.js"
+}, {
+  "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+  "url": "/static/js/2.83be525c.chunk.js.LICENSE.txt"
+}, {
+  "revision": "f4567141c1f2da13fd3d",
+  "url": "/static/js/main.69b4ccb3.chunk.js"
+}, {
+  "revision": "c7fc54348dd69e72515d",
+  "url": "/static/js/runtime-main.c1512f8e.js"
+}]);
---
+++ build/routes/data-router.js
@@ -0,0 +1,37 @@
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./firebase");
+
+var express = require("express");
+
+var router = express.Router();
+const collectionName = "responses";
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+let db = admin.firestore();
+router.get("/", function (req, res, next) {
+  let data = {};
+  data[collectionName] = {};
+  let results = db.collection(collectionName).get().then(snapshot => {
+    snapshot.forEach(doc => {
+      data[collectionName][doc.id] = doc.data();
+    });
+    return data;
+  }).catch(error => {
+    console.log(error);
+  });
+  results.then(dt => res.status(200).send(data));
+});
+router.post("/", function (req, res, next) {
+  console.log(JSON.stringify(req.body));
+  const data = JSON.parse(req.body.data);
+  const docId = data.id;
+  delete data.id;
+  db.collection(collectionName).doc(docId).set(data).then(() => res.status(200).send({
+    data: data
+  })).catch(() => res.status(200).send({
+    err: true
+  }));
+});
+module.exports = router;
